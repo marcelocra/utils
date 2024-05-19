d3ab30ab248c3372fed55dd00f1278bd1b299618
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2024-05-19 18:08:33 -0300

    add node_modules reference

--- bin.js
+++ bin.js
@@ -7,5 +7,6 @@ import { dirname, resolve } from "path";
 const __dirname = fileURLToPath(dirname(import.meta.url));
 
 addClassPath(resolve(__dirname, "src"));
+addClassPath(resolve(__dirname, "node_modules/mcra-utils"));
 
 await loadFile(resolve(__dirname, "src/dev/marcelocra/bin.cljs"));
