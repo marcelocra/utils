1ffa515c210aed0c8a9741e67d1fdff2085b234e
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2024-06-02 22:43:28 -0300

    get repl working in vscode

--- src/dev/marcelocra/core.cljs
+++ src/dev/marcelocra/core.cljs
@@ -1,8 +1,14 @@
 (ns dev.marcelocra.core
   (:require
-   ["node:fs" :as fs]))
+   ["node:fs" :as fs]
+   ["node:path" :as path]
+   ["shelljs$default" :as shell]))
 
 (set! *warn-on-infer* true)
+
+(fs/readdirSync
+ (path/resolve (-> (shell/pwd) (.toString))
+               "templates"))
 
 (defn something []
   (fs/readdirSync "."))
