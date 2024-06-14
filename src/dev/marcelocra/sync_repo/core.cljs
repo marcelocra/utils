1195cb4b8201a0563fd2ebb888b2540858d71d94
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2024-06-14 15:54:54 -0300

    restart playing with clojure

--- src/dev/marcelocra/sync_repo/core.cljs
+++ src/dev/marcelocra/sync_repo/core.cljs
@@ -4,7 +4,7 @@
    ["path" :as path]
    ["fs" :as fs]))
 
-(.-HOME js/process.env)
+(def debug true)
 
 (def logfile-path (-> js/process.env
                       (.-HOME)
@@ -15,4 +15,24 @@
   nil)
 
 (defn log [text-to-log]
-  (fs/writeFileSync logfile-path text-to-log))
+  (if debug
+    (println text-to-log)
+    (fs/writeFileSync logfile-path text-to-log)))
+
+(defn dump-log []
+  (fs/readFileSync logfile-path "utf8"))
+
+(defn now []
+  (-> (js/Date.)
+      (.toLocaleString "en-UK" #js{:day "numeric",
+                                   :weekday "short",
+                                   :month "short",
+                                   :year "numeric",
+                                   :hour "2-digit",
+                                   :hour12 false,
+                                   :minute "2-digit",
+                                   :second "2-digit",
+                                   :timeZone "America/Sao_Paulo"})))
+
+(log "\n")
+(log (now))
