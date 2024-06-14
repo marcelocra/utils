232426cdbf58d8f0d3563e4564083c7ce9ec66c0
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2024-06-14 14:25:57 -0300

    try again with deno

--- bin.ts
+++ bin.ts
@@ -2,15 +2,54 @@
 
 import { $ } from "zx";
 import { expect, test } from "bun:test";
-import path from "path";
+import path from "node:path";
+import { parseArgs } from "node:util";
 
-const DEFAULT_LOGFILE_PATH = path.resolve(Bun.env.HOME, ".sync-repo.log.txt");
+function checkValidEnv() {
+  if (!Bun || !Bun.env.NODE_ENV || !Bun.env["HOME"]) {
+    console.error(
+      "Please, provide both NODE_ENV and HOME environment variables.",
+    );
+
+    process.exit(1);
+  }
+
+  return {
+    nodeEnv: Bun.env.NODE_ENV,
+    isTest: Bun.env.NODE_ENV === "test",
+    home: Bun.env["HOME"],
+  };
+}
+
+function d(msg: any, opts = { hide: true }) {
+  if (opts.hide) {
+    return;
+  }
+
+  console.log(JSON.stringify(msg, null, 2));
+}
+
+const ENVS = checkValidEnv();
+const DEFAULT_LOGFILE_PATH = path.resolve(ENVS.home, ".sync-repo.log.txt");
 
 function helloWorld() {
   return "hello";
 }
 
-if (Bun.env.NODE_ENV === "test") {
+const { values, positionals } = parseArgs({
+  args: Bun.argv,
+  options: {
+    dir: {
+      type: "string",
+    },
+  },
+  strict: true,
+  allowPositionals: ENVS.isTest,
+});
+
+d({ values, positionals }, { hide: false });
+
+if (ENVS.isTest) {
   test(helloWorld.name, () => {
     expect(helloWorld()).toBe("hello");
   });
